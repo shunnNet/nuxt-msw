5544a6c09e37ba19cb8a5d17bb12a578ca4c2e64
net <wendell20904102@gmail.com>
2024-08-21 00:01:19 +0800

    chore: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ rm -rf src/runtime/dynamic
 
 pnpm nuxt-module-build build
 
-echo "recovering mockServiceWorker.js"
-mv dist/runtime/public/mockServiceWorker.mjs dist/runtime/public/mockServiceWorker.js
-rm -f dist/runtime/public/mockServiceWorker.d.ts
+# echo "recovering mockServiceWorker.js"
+# mv dist/runtime/public/mockServiceWorker.mjs dist/runtime/public/mockServiceWorker.js
+# rm -f dist/runtime/public/mockServiceWorker.d.ts
